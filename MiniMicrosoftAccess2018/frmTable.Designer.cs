3eb082880eaf99d9caea5b07c655b56cc6a929f7
Jerome Stewart <jeromerocks@live.ca>
2018-02-28 22:33:29 -0500

    Added form table

---
+++ MiniMicrosoftAccess2018/frmTable.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MiniMicrosoftAccess2018
+{
+    partial class frmTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.tabPageTb1 = new System.Windows.Forms.TabPage();
+            this.metroTabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.tabPageTb1);
+            this.metroTabControl1.Location = new System.Drawing.Point(23, 50);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(721, 469);
+            this.metroTabControl1.TabIndex = 0;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // tabPageTb1
+            // 
+            this.tabPageTb1.Location = new System.Drawing.Point(4, 38);
+            this.tabPageTb1.Name = "tabPageTb1";
+            this.tabPageTb1.Size = new System.Drawing.Size(713, 427);
+            this.tabPageTb1.TabIndex = 0;
+            this.tabPageTb1.Text = "Table 1";
+            // 
+            // frmTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 542);
+            this.Controls.Add(this.metroTabControl1);
+            this.Name = "frmTable";
+            this.Text = "Table Creation";
+            this.metroTabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private System.Windows.Forms.TabPage tabPageTb1;
+    }
+}
